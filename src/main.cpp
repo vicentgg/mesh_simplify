3dd24e79b2c6dab71d806b2284718ad0bacb57da
vicentgg <1342514547@qq.com>
2022-03-22 18:40:23 +0800

    first commit

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include <cmath>
 #include <queue>
 #include <algorithm>
+#include <iostream>
 #define BUFFER_SIZE 1024
 #define INFD 1e8
 #define EPSLOOSE 0.1
@@ -15,7 +16,8 @@ using std::min;
 using std::max;
 using std::make_pair;
 
-
+double w; //指数
+//定义一个顶点
 class Vector {
   double a[4];
   int _size;
@@ -57,14 +59,15 @@ public:
 };
 // typedef std::vector<double> Vector;
 typedef std::vector<Vector> Matrix;
-typedef std::pair<int, int> Edge;
+typedef std::pair<int, int> Edge; //定义边 数据为两个顶点的索引
 
+//输出一个顶点
 void printVector(const Vector &v) {
   for (int i = 0; i < v.size(); i++)
     printf("%.4lf\t", v[i]);
   printf("\n");
 }
-
+//输出一个顶点矩阵
 void printMatrx(const Matrix &m) {
   for (auto v : m)
     printVector(v);
@@ -152,10 +155,17 @@ Vector operator * (const double &a, const Vector &b) {
 }
 
 Vector operator / (const Vector &a, const double &b) {
-  assert(b != 0);
+  // assert(b != 0);
+
   Vector c(a.size());
-  for (int i = 0; i < a.size(); i++)
-    c[i] = a[i] / b;
+  for (int i = 0; i < a.size(); i++) {
+    if(b != 0) {
+      c[i] = a[i] / b;
+    }
+    else {
+      c[i] = 0;
+    }
+  }
   return c;
 }
 
@@ -183,15 +193,24 @@ Vector solveEquation(Matrix m, int n) {
   return v;
 }
 
+Matrix operator * (const Matrix &a, const double &cu) {
+  assert(a.size() != 0);
+  Matrix c(a.size());
+  for (int i = 0; i < a.size(); i ++) {
+    c[i] = cu * a[i];
+  }
+  return c;
+}
 
 class Model {
   std::vector< Vector > vertex;
   std::vector<bool> removed;
-  std::vector< std::set<Edge> > face;
+  std::vector< std::set<Edge> > face; 
   std::set<Edge> edge;
-  std::priority_queue< std::pair<double, Edge> > heap;
+  std::priority_queue< std::pair<double, Edge> > heap;  //记录一个小顶堆 存放每条边
   size_t faceN;
 
+  //返回边缘的长度
   double edgeLen(const Edge &e) {
     return norm(vertex[e.first] - vertex[e.second]);
   }
@@ -217,13 +236,14 @@ public:
     return faceN;
   }
 
+  //检查数据格式是否正确
   void selfCheck() {
     std::set<Edge> ss;
 
     size_t vertexN = getVertexN();
     for (int i = 0; i < vertexN; i++) {
-      if (removed[i]) {
-        assert(face[i].size() == 0);
+      if (removed[i]) { //如果这个顶点被移除了
+        assert(face[i].size() == 0); //检查这个
       } else {
         for (const auto &x : face[i]) {
           assert(!removed[x.first] && !removed[x.second]);
@@ -242,10 +262,9 @@ public:
     //(for models which are not closed)
   }
 
+  //加载obj模型数据
   void loadFromFile(std::string filename) {
     clear();
-
-
     char buffer[BUFFER_SIZE];
     FILE *file = fopen(filename.c_str(), "r");
     std::vector<std::string> vertexIn;
@@ -253,11 +272,11 @@ public:
     while (fgets(buffer, BUFFER_SIZE, file) != NULL) {
       int ptr = 0;
       while (buffer[ptr] != 0 && buffer[ptr] != 'v' && buffer[ptr] != 'f' && buffer[ptr] != '#') ptr++;
-      if (buffer[ptr] == 'v') vertexIn.push_back(std::string(buffer));
+      if (buffer[ptr] == 'v' && buffer[ptr+1] == ' ') vertexIn.push_back(std::string(buffer));
       if (buffer[ptr] == 'f') faceIn.push_back(std::string(buffer));
     }
     fclose(file);
-
+ 
 
     size_t vertexN = vertexIn.size();
     vertex.resize(vertexN, Vector(3, 0));
@@ -269,19 +288,40 @@ public:
       sscanf(vertexIn[i].c_str(), "%*s%lf%lf%lf", &vertex[i][0], &vertex[i][1], &vertex[i][2]);
     }
 
-
     for (const auto &f : faceIn) {
       int v[3];
+      // std::cout << f.c_str() << std::endl;
+      
+      // std::string faceindex;
+      // faceindex = f.c_str();
+      // for(int i = 0; i < faceindex.size(); i++) {
+      //   if(faceindex[i] == '/') {
+      //     int j = i;
+      //     while(faceindex[i] != ' ') {
+      //       i ++;
+      //     }
+      //     faceindex.erase(j,i-j);
+      //     i = j;
+      //   }
+      // }
+
+      // std::cout << faceindex << std::endl;
+      // const char* str = faceindex.c_str();
+
       sscanf(f.c_str(), "%*s%d%d%d", v, v + 1, v + 2);
       v[0] --; v[1] --; v[2] --;
       face[v[0]].insert(make_pair(v[1], v[2]));
       face[v[1]].insert(make_pair(v[2], v[0]));
       face[v[2]].insert(make_pair(v[0], v[1]));
       std::sort(v, v + 3);
+
       assert(0 <= v[0] && v[0] < v[1] && v[1] < v[2] && v[2] < vertexN);
+
       edge.insert(make_pair(v[0], v[1]));
       edge.insert(make_pair(v[1], v[2]));
       edge.insert(make_pair(v[0], v[2]));
+
+      
     }
   }
 
@@ -309,6 +349,7 @@ public:
     }
   }
 
+  //计算每条边收缩后的新顶点位置 和收缩的代价
   std::pair<Vector, double> getPosition(const Edge &e) {
     // std::set<int> neighbor1;
     // for (const auto &f : face[e.first]) {
@@ -331,56 +372,70 @@ public:
     //   return make_pair((vertex[e.first] + vertex[e.second]) / 2, INFD);
     // }
 
-    Matrix q(4, Vector(4, 0));
+    //先计算该边缘两个顶点的误差矩阵Q 计算结果相加即可
+    Matrix q1(4, Vector(4, 0));
     for (const auto &f : face[e.first]) {
       auto n = crossProduct(vertex[f.first] - vertex[e.first], vertex[f.second] - vertex[e.first]);
       n = n / norm(n);
       n.push_back(-innerProduct(vertex[e.first], n));
-      outerProductFast(n, n, q);
+      outerProductFast(n, n, q1);
     }
+
+    // double w = 0.3;
+
+    q1 = q1 * (1 + w);
+    
+    Matrix q2(4, Vector(4, 0));
     for (const auto &f : face[e.second]) {
       auto n = crossProduct(vertex[f.first] - vertex[e.second], vertex[f.second] - vertex[e.second]);
       n = n / norm(n);
       n.push_back(-innerProduct(vertex[e.second], n));
-      outerProductFast(n, n, q);
+      outerProductFast(n, n, q2);
     }
+
+    q2 = q2 * (1 - w);
+
+    Matrix q(4, Vector(4, 0));
+    q = q1 + q2;
 
     Vector v;
     try {
-      v = solveEquation(q, 3);
+      v = solveEquation(q, 3); //接着对二次误差矩阵进行偏导计算 找到最小二次误差值的新顶点
     } catch(...) {
-      v = (vertex[e.first] + vertex[e.second]) / 2;
+      v = (vertex[e.first] + vertex[e.second]) / 2; //如果这个二次误差矩阵不可逆 则选取中间的点
     }
     if (norm(v - vertex[e.first]) + norm(v - vertex[e.second]) > TOLERATE * norm(vertex[e.first] - vertex[e.second])) {
       v = (vertex[e.first] + vertex[e.second]) / 2;
     }
     v.push_back(1);
-    double cost = innerProduct(innerProduct(v, q), v);
+    double cost = innerProduct(innerProduct(v, q), v); // 计算新顶点的二次误差矩阵 即代价
     assert(cost > -EPS);
     v.pop_back();
     return make_pair(v, cost);
   }
 
+  //根据阈值 选择需要折叠的边缘进行收缩
   std::pair<Edge, Vector> selectEdge(double threshold) {
     Edge idx = make_pair(-1, -1);
     Vector pos;
     std::pair<double, Edge> tmp;
     while (!heap.empty()) {
-      tmp = heap.top();
+      tmp = heap.top(); 
       heap.pop();
-      if (edge.find(tmp.second) == edge.end()) continue;
-      if (removed[tmp.second.first] || removed[tmp.second.second]) continue;
-      if (edgeLen(tmp.second) > threshold) continue;
-      auto act = getPosition(tmp.second);
-      if (fabs(act.second + tmp.first) > EPS) continue;
-      idx = tmp.second;
-      pos = act.first;
+      if (edge.find(tmp.second) == edge.end()) continue;  //当不存在时
+      if (removed[tmp.second.first] || removed[tmp.second.second]) continue; //当其中一个顶点已经被移除
+      if (edgeLen(tmp.second) > threshold) continue; //当不满足阈值时 都跳过
+      auto act = getPosition(tmp.second); 
+      if (fabs(act.second + tmp.first) > EPS) continue; //如果生成的顶点代价不满足这个范围 跳过
+      idx = tmp.second; //去除的边缘
+      pos = act.first;  //新生成的顶点
       break;
     }
     printf("%lf %d %d", -tmp.first, idx.first, idx.second);
-    return std::make_pair(idx, pos);
+    return std::make_pair(idx, pos); //返回去除的边缘和新生成的顶点
   }
 
+  //边缘折叠后 判断面片是否翻转 
   bool faceReverse(const Edge &e, const Vector &v1, const Vector &v2) {
     const auto &x = vertex[e.first];
     const auto &y = vertex[e.second];
@@ -388,12 +443,13 @@ public:
     return 0;
   }
 
+  //按照边的收缩代价 加入到小顶堆中
   void addToHeap(const Edge &e, double threshold) {
     if (edgeLen(e) > threshold) return;
     auto pos = getPosition(e);
     heap.push(make_pair(-pos.second, e));
   }
-
+  //更新与收缩顶点相关的边的代价
   void updateNeighborEdge(int v, double threshold) {
     std::set<int> neighbor;
     for (const auto &f : face[v]) {
@@ -405,8 +461,10 @@ public:
     }
   }
 
+  //去除相关的边缘 
   void removeEdge(const Edge &e, const Vector &v, double threshold) {
     std::vector<Edge> toRev;
+    //遍历与该顶点相关的面片中的所有边
     for (const auto &f : face[e.first]) {
       if (f.first == e.second || f.second == e.second) continue;
       auto reverse = faceReverse(f, vertex[e.first], v);
@@ -486,6 +544,7 @@ public:
     }
   }
 
+  //小顶堆存放所有收缩对的代价 
   void buildHeap(double threshold) {
     while (!heap.empty()) heap.pop();
     for (const auto &e : edge) {
@@ -494,12 +553,13 @@ public:
   }
 
   void simplify(size_t target, double threshold) {
-    buildHeap(threshold);
-    while (faceN > target) {
-      printf("%c%zu ", 13, faceN);
-      auto e = selectEdge(threshold);
+    buildHeap(threshold); //建立一个小顶堆 存放所有收缩对的代价
+    
+    while (faceN > target) { //根据简化比例 开始简化
+      printf("%c%zu ", 13, faceN); //更新面片数量
+      auto e = selectEdge(threshold); //开始收缩
       if (e.first != make_pair(-1, -1))
-        removeEdge(e.first, e.second, threshold);
+        removeEdge(e.first, e.second, threshold); //去除相关的边缘 
       else {
         printf("%cERROR: No enough edges under threshold.\n", 13);
         printf("Warning: Current result will be save.\n");
@@ -512,6 +572,14 @@ public:
 
 } model;
 
+// 获取当前系统时间
+int64_t getCurrentLocalTimeStamp()
+{
+    std::chrono::time_point<std::chrono::system_clock, std::chrono::milliseconds> tp = std::chrono::time_point_cast<std::chrono::milliseconds>(std::chrono::system_clock::now());
+    auto tmp = std::chrono::duration_cast<std::chrono::milliseconds>(tp.time_since_epoch());
+    return tmp.count();
+}
+
 int main(int argc, char **argv) {
   if (argc < 4) {
     printf("Usage:\n ./main [Input Object] [Output Object] [Simplify Rate] [Threshold Value]");
@@ -521,8 +589,11 @@ int main(int argc, char **argv) {
   std::string outputModelFileName(argv[2]);
   double simplifyRate = atof(argv[3]);
   double threshold;
+
   if (argc == 5) {
-    threshold = atof(argv[4]);
+    // threshold = atof(argv[4]);
+    w = atof(argv[4]);
+    threshold = INFD;
   } else {
     printf("Warning: use threshold = INF (default)\n");
     threshold = INFD;
@@ -535,7 +606,7 @@ int main(int argc, char **argv) {
   printf("------------------------------------\n");
 
 
-  time_t start = time(0);
+  int64_t start = getCurrentLocalTimeStamp();
 
   model.loadFromFile(inputModelFileName);
 
@@ -549,7 +620,7 @@ int main(int argc, char **argv) {
 
   model.saveToFile(outputModelFileName);
   // model.selfCheck();
-  time_t end = time(0);
-  printf("%cSave to [%s] successfully. Time %ld sec.\n", 13, outputModelFileName.c_str(), end - start);
+  int64_t end = getCurrentLocalTimeStamp();
+  printf("%cSave to [%s] successfully. Time %lld ms.\n", 13, outputModelFileName.c_str(), end - start);
   return 0;
 }
